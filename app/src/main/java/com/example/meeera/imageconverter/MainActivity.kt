fdaa3af7b3eda95349f363ba9a22b93304c9a6b8
amitiwary999 <tiwarya9@gmail.com>
2017-12-20 13:25:30 +0530

    working on pdf to image conversion

--- app/src/main/java/com/example/meeera/imageconverter/MainActivity.kt
+++ app/src/main/java/com/example/meeera/imageconverter/MainActivity.kt
@@ -7,17 +7,16 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.pdf.PdfRenderer
 import android.net.Uri
-import android.os.AsyncTask
-import android.os.Build
+import android.os.*
 import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import com.afollestad.materialdialogs.MaterialDialog
 import com.gun0912.tedpicker.ImagePickerActivity
@@ -34,16 +33,23 @@ import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import org.apache.commons.io.FileUtils
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.rendering.ImageType
+import org.apache.pdfbox.rendering.PDFRenderer
+import org.apache.pdfbox.tools.imageio.ImageIOUtil
+import java.io.BufferedOutputStream
 
 class MainActivity : AppCompatActivity() {
 
     private val INTENT_REQUEST_GET_IMAGES = 11
-    val REQUEST_GET_DOC = 2
+    val REQUEST_GET_PDF = 2
     val REQUEST_ID_MULTIPLE_PERMISSIONS = 1
     val REQUEST_ID_MULTIPLE_PERMISSIONS1 = 2
     var tempUri : ArrayList<String> = ArrayList()
     var tempDocUri : ArrayList<String> = ArrayList()
+    var tempPdfUri : ArrayList<String> = ArrayList()
     var docUri : ArrayList<String> = ArrayList()
+    var pdfUri : ArrayList<String> = ArrayList()
     var imageUri : ArrayList<String> = ArrayList()
     var fileName : String = ""
     var docFilePath : String ?= null
@@ -61,6 +67,12 @@ class MainActivity : AppCompatActivity() {
         })
         docpdf.setOnClickListener({
             createDocPdf()
+        })
+        selectpdf.setOnClickListener({
+            selectPdf()
+        })
+        pdfimg.setOnClickListener({
+            createImage()
         })
     }
 
@@ -155,6 +167,74 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(Intent.createChooser(intent, "Select doc"), REQUEST_GET_DOC )*/
     }
 
+    fun selectPdf(){
+        var intent = Intent(this, NormalFilePickActivity::class.java)
+        intent.putExtra(NormalFilePickActivity.SUFFIX, arrayOf("pdf"))
+        startActivityForResult(intent, REQUEST_GET_PDF)
+    }
+
+    fun createImage(){
+        if(tempPdfUri.size == 0){
+            Toast.makeText(this, "No pdf selected", Toast.LENGTH_LONG).show()
+        }else{
+            pdfUri = tempPdfUri.clone() as ArrayList<String>
+            MaterialDialog.Builder(this)
+                    .title("Creating Pdf")
+                    .content("Enter File name")
+                    .input("Example : test", null, MaterialDialog.InputCallback { _, input ->
+                        if (input == null || input.toString().trim().equals("")) {
+                            Toast.makeText(this, "name cannot be blank", Toast.LENGTH_SHORT).show()
+                        } else {
+                            fileName = input.toString()
+                            Log.d("docsize", "size "+pdfUri.size)
+                            //CreatingPdfImg(this, fileName, pdfUri).execute()
+                            createimg(fileName, pdfUri)
+                            //CreatingDocPdf(this, fileName, docUri).execute()
+                        }
+                    }).show()
+        }
+    }
+
+    fun createimg(fileName:String, docUri:ArrayList<String>){
+        var imageUri : ArrayList<String> = docUri
+        var path : String = Environment.getExternalStorageDirectory().absolutePath+"/ImagePdf"
+        var storageDir = File(path)
+        storageDir.mkdirs()
+        // path = path + fileName + ".png"
+        for (i in 0 until imageUri.size){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                var file = File(imageUri[i])
+                var fileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY)
+                var renderer = PdfRenderer(fileDescriptor)
+                var filePath = storageDir.toString()+fileName
+                for(page in 0 until renderer.pageCount){
+                    var page = renderer.openPage(i)
+                    var pageWidth = page.width
+                    var pageHeight = page.height
+                    var bitmap = Bitmap.createBitmap(pageWidth, pageHeight, Bitmap.Config.ARGB_8888)
+                    page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+                    Log.d("amit", "bitmap1 "+bitmap)
+                    var out = FileOutputStream(filePath)
+                    var bos = BufferedOutputStream(out)
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 70, bos)
+                    img.setImageBitmap(bitmap)
+                    Log.d("amit", "bitmap "+bitmap)
+                    bos.flush()
+                    bos.close()
+                    page.close()
+                }
+                renderer.close()
+                fileDescriptor.close()
+                /* var document = PDDocument.load(File(imageUri[i]))
+                 var pdfRenderer = PDFRenderer(document)
+                 for (page in 0 until document.numberOfPages) {
+
+                     // ImageIOUtil.writeImage(bim, String.format(""))
+                 }*/
+            }
+        }
+    }
+
     fun createDocPdf(){
         if(tempDocUri.size == 0) {
             Toast.makeText(this, "No doc selected", Toast.LENGTH_LONG).show()
@@ -192,6 +272,14 @@ class MainActivity : AppCompatActivity() {
                 tempDocUri.add(list[i].path)
             }
             Toast.makeText(this, "Doc Added", Toast.LENGTH_SHORT).show()
+        }
+
+        if(requestCode == REQUEST_GET_PDF && resultCode == Activity.RESULT_OK){
+            var list : ArrayList<NormalFile> = data!!.getParcelableArrayListExtra(Constant.RESULT_PICK_FILE)
+            for(i in list.indices){
+                tempPdfUri.add(list[i].path)
+            }
+            Toast.makeText(this, "Pdf Added", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -382,6 +470,67 @@ class MainActivity : AppCompatActivity() {
             super.onPostExecute(result)
             MainActivity().docUri.clear()
             MainActivity().tempDocUri.clear()
+            dialog.dismiss()
+        }
+    }
+
+    class CreatingPdfImg(context: Activity, fileName:String, docUri:ArrayList<String>) : AsyncTask<String, String, String>(){
+
+        var fileName : String = fileName
+        var imageUri : ArrayList<String> = docUri
+        var builder : MaterialDialog.Builder  = MaterialDialog.Builder(context)
+                .title("please wait")
+                .content("Creating File")
+                .cancelable(false)
+                .progress(true, 0)
+        var dialog : MaterialDialog = builder.build()
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            dialog.show()
+        }
+
+        override fun doInBackground(vararg params: String?): String {
+            var path : String = Environment.getExternalStorageDirectory().absolutePath+"/ImagePdf"
+            var storageDir = File(path)
+            storageDir.mkdirs()
+           // path = path + fileName + ".png"
+            for (i in 0 until imageUri.size){
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    var file = File(imageUri[i])
+                    var fileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY)
+                    var renderer = PdfRenderer(fileDescriptor)
+                    var filePath = storageDir.toString()+fileName
+                    for(page in 0 until renderer.pageCount){
+                        var page = renderer.openPage(i)
+                        var pageWidth = page.width
+                        var pageHeight = page.height
+                        var bitmap = Bitmap.createBitmap(pageWidth, pageHeight, Bitmap.Config.ARGB_8888)
+                        page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+                        Log.d("amit", "bitmap1 "+bitmap)
+                        var out = FileOutputStream(filePath)
+                        var bos = BufferedOutputStream(out)
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 70, bos)
+                        Log.d("amit", "bitmap "+bitmap)
+                        bos.flush()
+                        bos.close()
+                        page.close()
+                    }
+                    renderer.close()
+                    fileDescriptor.close()
+                   /* var document = PDDocument.load(File(imageUri[i]))
+                    var pdfRenderer = PDFRenderer(document)
+                    for (page in 0 until document.numberOfPages) {
+
+                        // ImageIOUtil.writeImage(bim, String.format(""))
+                    }*/
+                }
+            }
+            return ""
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
             dialog.dismiss()
         }
     }
